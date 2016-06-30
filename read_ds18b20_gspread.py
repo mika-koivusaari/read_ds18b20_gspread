584218676cb92c89aa78a6948d28374ff72ee47a
Mika Koivusaari <mik@mikki.lan>
2016-06-30 21:15:51 +0300

    Write header and data to google sheets

--- read_ds18b20_gspread.py
+++ read_ds18b20_gspread.py
@@ -10,15 +10,25 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name('solar shield dat
 gc = gspread.authorize(credentials)
 
 wks = gc.open_by_key('1V9OOICml3TB_kz-c6pKPEQZLdh8nlp5JI1Lb55njau8').sheet1
-
 sensors=('28.21CFCE010000','28.79C1CE010000','28.838ECE010000')
+row=1
+col=1
+value=wks.cell(row,col).value
+if value=='':
+  wks.update_cell(row,1,'time')
+  wks.update_cell(row,2,sensors[0])
+  wks.update_cell(row,3,sensors[1])
+  wks.update_cell(row,4,sensors[2])
+
+while wks.cell(row,1).value!='':
+  row=row+1
+
 owfspath='/var/1-wire/mnt/1F.BD5C08000000/aux/'
 
 f=open(owfspath+'simultaneous/temperature','w')
 f.write('1')
 f.close()
-row=1
-col=1
+
 for sensor in sensors:
   try:
     f=open(owfspath+sensor+'/temperature','r')
