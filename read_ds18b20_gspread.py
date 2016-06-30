4c323e7f41a31f171b3660e5c33cd8989dd87990
Mika Koivusaari <mik@mikki.lan>
2016-06-30 13:16:10 +0300

    Simultaneous read

--- read_ds18b20_gspread.py
+++ read_ds18b20_gspread.py
@@ -3,11 +3,15 @@ import sys
 
 sensors=('28.21CFCE010000','28.79C1CE010000','28.838ECE010000')
 owfspath='/var/1-wire/mnt/1F.BD5C08000000/aux/'
+
+f=open(owfspath+'simultaneous/temperature','w')
+f.write('1')
+f.close()
 for sensor in sensors:
   try:
-    f=open(owfspath+sensors+'/temperature','r')
+    f=open(owfspath+sensor+'/temperature','r')
     value=f.readline()
-    print "sensor",sensors,value
+    print "sensor",sensor,value
 #    sys.stderr.write("id=%i value='%s'\n" % (record[0],value))
 #    cursor.execute("INSERT INTO data (sensorid,time,value) VALUES (%s,now(), truncate(%s,2))",(record[0],value.lstrip()))
   except IOError:
