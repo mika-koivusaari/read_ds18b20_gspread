1c0af78bf0ff6e6c614a473aaf401a7b56c5f007
Mika Koivusaari <mik@mikki.lan>
2016-07-05 20:34:02 +0300

    Added prints to indicate progress.

--- read_ds18b20_csv.py
+++ read_ds18b20_csv.py
@@ -11,18 +11,18 @@ owfspath='/var/1-wire/mnt/1F.BD5C08000000/aux/'
 
 #write to simultaneous so all sensors do their conversion at the same time
 #all sensors in the bus need to be externally powered! Parasitic sensors can only convert one at a time!
-f=open(owfspath+'simultaneous/temperature','w')
-f.write('1')
-f.close()
+#f=open(owfspath+'simultaneous/temperature','w')
+#f.write('1')
+#f.close()
 
 #loop all sensors and put their values in a list
 for sensor in sensors:
   try:
-    f=open(owfspath+sensor+'/temperature','r')
+    f=open(owfspath+sensor+'/temperature12','r')
     value=f.readline()
     valuef=float(value)
 #    sys.stdout.write('%0.4f, ' % (valuef))
     values.append(valuef)
   except IOError:
     print "sensor not found ",owfspath+sensor
-print '%s;%0.4f;%0.4f;%0.4f' % (values[0],values[1],values[2])
+print '%s;%0.4f;%0.4f;%0.4f' % (_time,values[0],values[1],values[2])
--- write_data_gspread.py
+++ write_data_gspread.py
@@ -2,6 +2,7 @@
 import sys
 from datetime import date, timedelta
 import csv
+import re
 
 import gspread
 from gspread.exceptions import WorksheetNotFound
@@ -11,28 +12,43 @@ scope = ['https://spreadsheets.google.com/feeds']
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name('solar shield data-e15f87bae25f.json', scope)
 
-gc = gspread.authorize(credentials)
+#gc = gspread.authorize(credentials)
+headers = gspread.httpsession.HTTPSession(headers={'Connection':'Keep-Alive'})
+gc = gspread.Client(auth=credentials,http_session=headers)
+gc.login()
+
+print "Logged in."
 
 filename=None
 if len(sys.argv)==2:
   filename=sys.argv[1]
+  print "Filename given as argument "+filename
 else:
-  exit(1)
-  #yesterday = date.today() - timedelta(1)
-  #_date=.strftime('%d.%m')
+  yesterday = date.today() - timedelta(1)
+  filename='data/radiation_shield_%s.csv' % (yesterday.strftime('%Y%m%d'))
+  print "No filename given, using "+filename
+
+m = re.match(r".*radiation_shield_(\d{4})(\d{2})(\d{2}).csv", filename)
+year=m.group(1)
+month=m.group(2)
+day=m.group(3)
 
 csvfile=open(filename,'rb')
 csvreader=csv.reader(csvfile,delimiter=';')
 
-sheetname='testi'
+print "CSV file open."
+sheetname=year+'.'+month+'.'+day
 
 #open google sheets
 sheet = gc.open_by_key('1V9OOICml3TB_kz-c6pKPEQZLdh8nlp5JI1Lb55njau8')
+print "Google Sheet opened."
 #try to open todays sheet, if not succesfull then create it 
 try:
   wks=sheet.worksheet(sheetname)
+  print "Worksheet "+sheetname+" allready exists. Opened."
 except WorksheetNotFound:
   wks=sheet.add_worksheet(title=sheetname, rows="1441", cols="5")
+  print "Worksheet "+sheetname+" does not exists. Created."
 
 sensors=['28.21CFCE010000','28.79C1CE010000','28.838ECE010000']
 values=[]
